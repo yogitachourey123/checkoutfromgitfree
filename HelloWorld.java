768236706ccb7cd5b45e74d0552a052bd303c6f0
yogitachourey123 <111501961+yogitachourey123@users.noreply.github.com>
2022-09-23 10:24:20 +0530

    modifying

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
  class HelloWorld{
         public static void main(String[] args){
                  System.out.println("duncky");
+         System.out.println("welcome in devops batch");
+         
         }
         }
